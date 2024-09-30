6a555f18421d4d3d9b4bd315dd0bf2cf9481ae76
danimar-g <danielamar.gerard@gmail.com>
2024-09-30 17:32:06 -0400

    vista taxis

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Application, Request, Response } from 'express';
+import {PrismaClient } from '@prisma/client';
+
+const app: Application = express();
+const prisma = new PrismaClient();
+const PORT: number = 3001;
+
+app.use(express.json());
+app.use('/', async (req: Request, res: Response): Promise<void> => {
+    const taxis = await prisma.taxis.findMany() 
+    res.json(taxis);
+});
+
+app.listen(PORT, (): void => {
+    console.log('SERVER IS UP ON PORT:', PORT);
+});
