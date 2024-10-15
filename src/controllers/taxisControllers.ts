f6cae3c2c10b2a9a149e4a534e1f2e4a48f05793
DanielaMG <danielamar.gerard@gmail.com>
2024-10-15 14:14:10 -0400

    listo h3 y 4

--- src/controllers/taxisControllers.ts
+++ src/controllers/taxisControllers.ts
@@ -1,45 +1,28 @@
+// taxiController.ts
 import { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { getTaxis } from '../models/taxisModel';
 
 export const getAllTaxis = async (req: Request, res: Response): Promise<void> => {
     try {
         const page = parseInt(req.query.page as string) || 1;
         const limit = parseInt(req.query.limit as string) || 10;
-        const plate = req.query.plate as string; // Obtiene plate
+        const plate = req.query.plate as string;
 
-        if (isNaN(page) || page < 1) {
-            res.status(400).json({ error: 'pagina o limite invalido' });
-            return; 
-        }
-
-        if (isNaN(limit) || limit < 1) {
-            res.status(400).json({ error: 'pagina o limite invalido' });
+        if (isNaN(page) || page < 1 || isNaN(limit) || limit < 1) {
+            res.status(400).json({ error: 'Página o límite inválido' });
             return;
         }
-        const taxis = await prisma.taxis.findMany({
-            skip: (page - 1) * limit,
-            take: limit,
-            where: plate ? {
-                plate: {
-                    startsWith: plate,   
-                    mode: 'insensitive',
-                },
-            } : undefined,
-            select: {
-                id: true,
-                plate: true,
-            },
-        });
+
+        const taxis = await getTaxis(page, limit, plate);
 
         if (taxis.length === 0) {
-            res.status(400).json({ error: 'Página no encontrada' });
-            return;//sale de la fx sin retornar valor
+            res.status(404).json({ error: 'Página no encontrada' });
+            return;
         }
-        
+
         res.json(taxis);
     } catch (error) {
+        console.error(error);
         res.status(400).json({ error: 'Error al obtener los registros de taxis' });
     }
 };
--- src/models/taxisModel.ts
+++ src/models/taxisModel.ts
@@ -1,4 +1,21 @@
-export interface Taxi {
-    id: number;
-    plate: string;
-}
+// taxiModel.ts
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const getTaxis = async (page: number, limit: number, plate?: string) => {
+    return await prisma.taxis.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        where: plate ? {
+            plate: {
+                startsWith: plate,
+                mode: 'insensitive',
+            },
+        } : undefined,
+        select: {
+            id: true,
+            plate: true,
+        },
+    });
+};
